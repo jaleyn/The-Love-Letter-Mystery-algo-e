ac980262e6f185a310e59997b368b25ce25a62b1
Jallyn <jallynribaya979@gmail.com>
2025-10-31 13:14:36 +0800

    Create The Love-Letter Mystery.cs

---
+++ The Love-Letter Mystery.cs
@@ -0,0 +1,61 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'theLoveLetterMystery' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static int theLoveLetterMystery(string s)
+    {
+        int operations = 0;
+        int n = s.Length;
+
+        for (int i = 0; i < n / 2; i++)
+        {
+            
+            operations += Math.Abs(s[i] - s[n - i - 1]);
+        }
+
+        return operations;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int q = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int qItr = 0; qItr < q; qItr++)
+        {
+            string s = Console.ReadLine();
+
+            int result = Result.theLoveLetterMystery(s);
+
+            textWriter.WriteLine(result);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
